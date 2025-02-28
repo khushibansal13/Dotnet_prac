111a296dee94e5197fa8c3eccc4f9e263545fdba
khushibansal13 <khushibansal1302@gmail.com>
2025-02-28 15:47:56 +0530

    Refactor StockController to use asynchronous methods for database operations

--- api/Program.cs
+++ api/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+} );
 
 var app = builder.Build();
 
