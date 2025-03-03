62ceefbfb71f465e556544514c65f8b39be5eac8
khushibansal13 <khushibansal1302@gmail.com>
2025-03-03 11:07:16 +0530

    Add Comment functionality with repository pattern; implement CommentController and related DTOs

---
+++ api/Mappers/CommentMapper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Dtos.Comment;
+using api.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace api.Mappers
+{
+    public static class CommentMapper
+    {
+        public static CommentDto ToCommentDto(this Comment CommentModel)
+        {
+            return new CommentDto 
+            {
+                Id = CommentModel.Id,
+                Title = CommentModel.Title,
+                Content = CommentModel.Content,
+                CreatedOn = CommentModel.CreatedOn,
+                StockId = CommentModel.StockId
+            };
+        }
+    }
+}
