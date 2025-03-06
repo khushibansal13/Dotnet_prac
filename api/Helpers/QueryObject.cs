08ec2796b61d23b5f442ed72ff75065353d66c7e
khushibansal13 <khushibansal1302@gmail.com>
2025-03-06 14:16:08 +0530

    Add sorting functionality to stock retrieval

--- api/Helpers/QueryObject.cs
+++ api/Helpers/QueryObject.cs
@@ -8,6 +8,8 @@ namespace api.Helpers
     public class QueryObject
     {
         public string? Symbol { get; set; } = null;
-        public string? CompanyName { get; set; } = null;    
+        public string? CompanyName { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDecending { get; set; } = false;
     }
 }
